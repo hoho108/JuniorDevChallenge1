c305f22344ae6dd926a118b471b2e536d460ab75
hoho108 <asuna8406@gmail.com>
2025-03-05 11:16:53 +0800

    重構學生名單和成績計算邏輯
    將學生名單的獲取和成績計算的邏輯從 `Program` 類別中移除，並移至新的 `StudentService` 類別中。新增的 `GetStudentList` 方法負責獲取學生姓名和成績，而 `GetStudentScore` 方法則計算並顯示每位學生的成績。此重構提升了程式碼的可讀性和可維護性，並遵循了單一職責原則。

---
+++ StudentService.cs
@@ -0,0 +1,59 @@
+﻿namespace JuniorDevChallenge1
+{
+    internal class StudentService
+    {
+
+        public List<Student> GetStudentList()
+        {
+            var studentNamesList = new List<Student>();
+            var continueInput = "Y";
+
+            do
+            {
+                Console.WriteLine("請輸入學生姓名: ");
+                string? studentName = Console.ReadLine(); // 讀取學生姓名
+
+                if (string.IsNullOrEmpty(studentName))
+                {
+                    Console.WriteLine("學生姓名不能為空，請重新輸入");
+                    continue;
+                }
+
+                try
+                {
+                    Console.WriteLine("請輸入學生成績: ");
+                    int studentScore = Convert.ToInt32(Console.ReadLine());
+                    while (studentScore < 0 || studentScore > 100)
+                    {
+                        Console.WriteLine("輸入成績超過範圍，請重新輸入");
+                        studentScore = Convert.ToInt32(Console.ReadLine());
+                    }
+                    studentNamesList.Add(new Student(studentName, studentScore));
+                    Console.WriteLine("是否要輸入另一位學生？(Y/N)");
+                    continueInput = Console.ReadLine();
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("輸入的成績格式錯誤，請重新輸入");
+                }
+
+            } while (continueInput == "Y" || continueInput == "y");
+
+            return studentNamesList;
+        }
+
+        public void GetStudentScore(List<Student> studentNamesList)
+        {
+            for (int i = 0; i < studentNamesList.Count; i++)
+            {
+                // 三元表達式 ?若非a等於b
+                string grade = studentNamesList[i].Score >= 90 ? "A" :
+                               studentNamesList[i].Score >= 80 ? "B" :
+                               studentNamesList[i].Score >= 70 ? "C" :
+                               studentNamesList[i].Score >= 60 ? "D" : "F";
+
+                Console.WriteLine($"{studentNamesList[i].Name} 的成績是 {grade}");
+            }
+        }
+    }
+}
